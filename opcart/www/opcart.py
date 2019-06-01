a8bc6a8a5e923793cc452f7f20aa7259f2dad74c
ujjwal kumar <pathakujjwal93@gmail.com>
2019-06-01 16:39:16 +0530

    introduced integer

--- opcart/www/opcart.py
+++ opcart/www/opcart.py
@@ -7,16 +7,19 @@ from erpnext.shopping_cart.doctype.shopping_cart_settings.shopping_cart_settings
 from frappe.utils.nestedset import get_root_of
 from erpnext.accounts.utils import get_account_name
 from erpnext.utilities.product import get_qty_in_stock
-from erpnext.portal.product_configurator.utils import (get_products_for_website, get_product_settings,
-	get_field_filter_data, get_attribute_filter_data)
+
 
 sitemap = 1
 
 def get_context(context):
 	items = frappe.get_all('Item', filters={'show_in_website':1 }, fields=['item_name', 'item_code', 'item_group'])
 	context.items=items
-	context.item_name=items.item_name
-	context.item_code=items.item_code
+	#context.item_name=items.item_name
+	#context.item_code=items.item_code
+	qty=frappe.get_all("Stock Ledger Entry", {'item_code':items.item_code} , "qty_after_transaction")
+	context.qty=items.qty
+
+
 
 def set_cart_count(quotation=None):
 	if cint(frappe.db.get_singles_value("Shopping Cart Settings", "enabled")):
